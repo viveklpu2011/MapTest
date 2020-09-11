20b53ee93a2567119d18155b1f8f80a0b9892493
beesolver-source <59913321+beesolver-source@users.noreply.github.com>
2020-09-11 10:04:19 +0530

    viewmodel changes

--- XamarinFormsXaminals/Xaminals/ViewModels/HomeViewModel.cs
+++ XamarinFormsXaminals/Xaminals/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Plugin.Geolocator;
 using Plugin.Toasts;
 using System;
 using System.Collections.Generic;
@@ -6,12 +7,15 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xaminals.Controls;
 using Xaminals.Models;
 using Xaminals.Views;
+using XF.Material.Forms.UI.Dialogs;
 
 namespace Xaminals.ViewModels
 {
-     
+
     public class HomeViewModel : BaseViewModel
     {
         private INavigation navigation;
@@ -19,14 +23,81 @@ namespace Xaminals.ViewModels
         {
             this.navigation = navigation;
 
+
+            loadLatLng();
+        }
+
+        public CustomMap customMap { get; private set; }
+        public async void loadLatLng()
+        {
+            customMap = new CustomMap();
+            var loadingDialog = await MaterialDialog.Instance.LoadingDialogAsync(message: "Please Wait..");
+            try
+            {
+
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync(10000);
+
+
+                var start_pin = new CustomPin
+                {
+                    Pin = new Pin
+                    {
+                        Type = PinType.Place,
+                        Position = new Position(Convert.ToDouble(position.Latitude), Convert.ToDouble(position.Longitude)),
+                        Label = "",
+                        Address = ""
+                    },
+                    Id = "Xamarin",
+                    startPin = true
+                };
+                customMap.CustomPins = new List<CustomPin> { start_pin };
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    customMap.Pins.Add(start_pin.Pin);
+
+                }
+                customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(position.Latitude), Convert.ToDouble(position.Longitude)), Xamarin.Forms.Maps.Distance.FromKilometers(10)));
+
+                await loadingDialog.DismissAsync();
+            }
+            catch (Exception ex)
+            {
+                await loadingDialog.DismissAsync();
+            }
+
+        }
+
+
+        public async void searchLocation()
+        {
+            if (HomePage.Lat != 0 && HomePage.Lng != 0)
+            {
+                var start_pin = new CustomPin
+                {
+                    Pin = new Pin
+                    {
+                        Type = PinType.Place,
+                        Position = new Position(Convert.ToDouble(HomePage.Lat), Convert.ToDouble(HomePage.Lng)),
+                        Label = "",
+                        Address = ""
+                    },
+                    Id = "Xamarin",
+                    startPin = true
+                };
+                customMap.CustomPins = new List<CustomPin> { start_pin };
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    customMap.Pins.Add(start_pin.Pin);
+
+                }
+                customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(HomePage.Lat), Convert.ToDouble(HomePage.Lng)), Xamarin.Forms.Maps.Distance.FromKilometers(10)));
+            }
+
         }
 
 
 
-
-
-       
-      
         public Command LogoutCommand
         {
             get
--- XamarinFormsXaminals/Xaminals/Views/HomePage.xaml.cs
+++ XamarinFormsXaminals/Xaminals/Views/HomePage.xaml.cs
@@ -17,78 +17,20 @@ namespace Xaminals.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        HomeViewModel homeViewModel;
         public static double Lat = 0;
         public static double Lng = 0;
         public HomePage()
         {
             InitializeComponent();
-            BindingContext = new HomeViewModel(Navigation);
-            loadLatLng();
+            homeViewModel = new HomeViewModel(Navigation);
+            BindingContext = homeViewModel;
         }
-        public async void loadLatLng()
-        {
-            var loadingDialog = await MaterialDialog.Instance.LoadingDialogAsync(message: "Please Wait..");
-            try
-                {
-
-                    var locator = CrossGeolocator.Current;
-                   // locator.AllowsBackgroundUpdates = true;
-                    var position = await locator.GetPositionAsync(10000);
-
-
-                    var start_pin = new CustomPin
-                    {
-                        Pin = new Pin
-                        {
-                            Type = PinType.Place,
-                            Position = new Position(Convert.ToDouble(position.Latitude), Convert.ToDouble(position.Longitude)),
-                            Label = "",
-                            Address = ""
-                        },
-                        Id = "Xamarin",
-                        startPin = true
-                    };
-                    customMap.CustomPins = new List<CustomPin> { start_pin };
-                    if (Device.OS == TargetPlatform.iOS)
-                    {
-                        customMap.Pins.Add(start_pin.Pin);
-
-                    }
-                    customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(position.Latitude), Convert.ToDouble(position.Longitude)), Xamarin.Forms.Maps.Distance.FromKilometers(10)));
-
-                    await loadingDialog.DismissAsync();
-                }
-                catch (Exception ex)
-                {
-                    await loadingDialog.DismissAsync();
-                }
-            
-        }
+         
 
         protected override void OnAppearing()
         {
-            if (Lat != 0 && Lng != 0)
-            {
-                var start_pin = new CustomPin
-                {
-                    Pin = new Pin
-                    {
-                        Type = PinType.Place,
-                        Position = new Position(Convert.ToDouble(Lat), Convert.ToDouble(Lng)),
-                        Label = "",
-                        Address = ""
-                    },
-                    Id = "Xamarin",
-                    startPin = true
-                };
-                customMap.CustomPins = new List<CustomPin> { start_pin };
-                if (Device.OS == TargetPlatform.iOS)
-                {
-                    customMap.Pins.Add(start_pin.Pin);
-
-                }
-                customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(Lat), Convert.ToDouble(Lng)), Xamarin.Forms.Maps.Distance.FromKilometers(10)));
-            }
+            homeViewModel.searchLocation();
         }
 
     }
